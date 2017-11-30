e6b1da4060882026a94debcfa3dcee31d614a9a6
Andres Noe Garcia Medrano <andres@Andress-MacBook-Pro.local>
2017-11-29 22:26:50 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Authentitacion with passport and jwt
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+
+//DB Setuo
+mongoose.connect('mongodb://localhost/auth', { useMongoClient: true});
+// App setup;
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*'}));
+router(app);
+// a comment!!
+
+
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server Listening');
